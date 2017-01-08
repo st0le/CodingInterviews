8a288ee6ea8cf66ab615c15fd6d150a0a75cbbb2
Gaurav Kamath <gaurav.kamath108+webapps@gmail.com>
2017-01-07 22:15:09 -0800

    Added Question 15

--- CodingInterviews/Chapter3/Question15.cs
+++ CodingInterviews/Chapter3/Question15.cs
@@ -9,8 +9,6 @@ namespace CodingInterviews.Chapter3
         /*
          * Question 15 Please implement a function to merge two sorted lists into a single sorted list. 
          */
-
-
         public LLNode Merge(LLNode A, LLNode B)
         {
             LLNode nhead = null, last = null;
@@ -49,12 +47,38 @@ namespace CodingInterviews.Chapter3
             return nhead;
         }
 
+        public LLNode MergeRecurse(LLNode A,LLNode B)
+        {
+            if (A == null) return B;
+            if (B == null) return A;
+            
+            if(A.Data < B.Data)
+            {
+                A.Next = Merge(A.Next, B);
+                return A;
+            }else
+            {
+                B.Next = Merge(A, B.Next);
+                return B;
+            }
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
             LLNode A = LLNode.MakeList(1, 3, 5, 7);
             LLNode B = LLNode.MakeList(2, 4, 6, 8);
             LLNode C = Merge(A, B);
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, C.ToArray());
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            LLNode A = LLNode.MakeList(1, 3, 5, 7);
+            LLNode B = LLNode.MakeList(2, 4, 6, 8);
+            LLNode C = MergeRecurse(A, B);
             CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, C.ToArray());
 
         }
